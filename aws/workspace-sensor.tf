73d71249fab0f6eac3c2c9f89014cd4e47aed7eb
bmp-dot <bmpeterson@gmail.com>
2023-04-26 15:10:46 -0600

    Create workspace-sensor.tf

---
+++ aws/workspace-sensor.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    restapi = {
+      source = "Mastercard/restapi"
+      version = "1.18.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+  profile = var.AWSprofile
+}
+
+provider "restapi" {
+  alias               = "restapi_headers"
+  uri                 = var.BrainURI
+  headers =  {
+    Authorization     = "Token ${var.APIauth}"
+  }
+  insecure = true
+  write_returns_object = true
+  update_method = "POST"
+}
+
+resource "restapi_object" "sensor" {
+  provider = restapi.restapi_headers
+  path = "/api/v2.2/sensor_token"
+  data = "{}"
+  id_attribute = "result"
+  object_id = "update_sensor_token"
+}
+
+ resource "aws_cloudformation_stack" "nlb_stack" {
+  name = "${var.baseName}-nlb-${terraform.workspace}"
+  parameters = {
+    baseName          = "${var.baseName}-nlb-${terraform.workspace}"
+    subnets           = var.trafficSubnet
+  }
+
+ template_url = var.templateNLB
+
+}
+
+resource "aws_cloudformation_stack" "sensor_stack" {
+  name = "${var.baseName}-sensor-${terraform.workspace}"
+  parameters = {
+    baseName          = "${var.baseName}-sensor-${terraform.workspace}"
+    brainIP           = var.brainIP
+    instanceType      = var.instanceType
+    mgtPrivateIP      = var.mgtPrivateIP
+    mgtSecurityGroup  = var.mgtSecurityGroup
+    mgtSubnet         = var.mgtSubnet
+    mgtVpc            = var.mgtVpc
+    networkLB         = aws_cloudformation_stack.nlb_stack.outputs.LoadBalancerARN
+#    networkLB         = var.networkLB
+    registrationToken = restapi_object.sensor.api_data.token
+    sshKey            = var.sshKey
+    tenancy           = var.tenancy
+    trafficPrivateIP  = var.trafficPrivateIP
+    trafficSecurityGroup = var.trafficSecurityGroup
+    trafficSubnet     = var.trafficSubnet
+    trafficVpc        = var.trafficVpc
+  }
+
+  template_url = var.templateSensor
+
+}
+
+output "nlb_arn" {
+  value = aws_cloudformation_stack.nlb_stack.outputs.LoadBalancerARN
+}
+
+output "sensor_instance_id" {
+  value = aws_cloudformation_stack.sensor_stack.outputs.InstanceId
+}
+
+output "sensor_private_mgt_ip" {
+  value = aws_cloudformation_stack.sensor_stack.outputs.PrivateManagementIP
+}
+
+output "token_used" {
+  value = restapi_object.sensor.api_data.token
+}
